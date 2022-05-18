0b45620d47ce1dd7d3fdbd6710621cc2399aa80c
Aaron Russell <cpl.adrusselljr@gmail.com>
2022-05-18 10:18:37 -0400

    answered questions

--- Questions-JS.js
+++ Questions-JS.js
@@ -1,27 +1,44 @@
 const x = 6
 
-// 7. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x" using without using arrow functions.
+// 7. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x" without using arrow functions.
+function seven(a, b) {
+  let sum = a + b
+  return sum + x
+}
+// console.log(seven(2, 2))
 
 // 8. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x", using arrow functions.
+const eight = (a, b) => {
+  let sum = a + b
+  return sum + x
+}
+eight(2, 2)
 
 // 9. Write a function that returns another function. (use arrow functions please)
-
+const greeting = personName => {
+  console.log(`Hello ${personName}`)
+}
+const userGreeting = callback => {
+  const personName = "Aaron"
+  callback(personName)
+}
+userGreeting(greeting)
 
 // 10. given the following code:
 
 
 const getFunction = () => {
-  const y = 5;
+  const y = 5
 
-  const insideFunc = (a) => y + a;
+  const insideFunc = (a) => y + a
 
-  return insideFunc;
-};
+  return insideFunc
+}
 
 console.log(getFunction()(2))
 
 // Explain why the function that returns from getFunction still has access to "y" if "y" is not a global variable.
-
+// 10. because y is still within the same scope
 
 
 // 11. write a function that takes "couldThrowError()" as a callback argument.
@@ -29,10 +46,20 @@ console.log(getFunction()(2))
 // Make sure to handle errors that could be thrown by "couldThrowError()"
 
 const couldThrowError = () => {
-  
-  if(Math.ceil(Math.random() * 2) < 2){
-    throw new Error("Error was thrown");
+  if (Math.ceil(Math.random() * 2) < 2) {
+    throw new Error("Error was thrown")
   }
-  
-  return 'success'
+
+  return "success"
 }
+
+const isGreaterThanTwo = (callback) => {
+  try {
+    let x = callback()
+
+    console.log("\nisGreaterThanTwo try:", x)
+  } catch (e) {
+    console.log("\nisGreaterThanTwo catch:", e.message)
+  }
+}
+isGreaterThanTwo(couldThrowError)
